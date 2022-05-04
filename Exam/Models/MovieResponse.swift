26dd41970deb4121dd73df733c708ef9e95eea71
Carlos Caraccia <ccaraccia@ccaraccia-210MD6R.dyn.pinadmin.com>
2022-05-04 08:26:05 -0300

    Added models

---
+++ Exam/Models/MovieResponse.swift
@@ -0,0 +1,14 @@
+//
+//  MovieResponse.swift
+//  Exam
+//
+//  Created by Carlos Caraccia on 04/05/2022.
+//
+
+import Foundation
+
+struct MovieApiResponse: Decodable {
+    let movies:[Movie]
+}
+
+
