1ce553f997aaa9e6ecbc4d6e4dfced39bedc3b06
Carlos Caraccia <ccaraccia@ccaraccia-210MD6R.dyn.pinadmin.com>
2022-05-08 12:30:19 -0300

    Added initial collection view with fake cell

---
+++ Exam/TMDBInitialInitialMoviesCell.swift
@@ -0,0 +1,69 @@
+//
+//  TMDBInitialInitialMovieCell.swift
+//  Exam
+//
+//  Created by Carlos Caraccia on 08/05/2022.
+//
+
+import Foundation
+import UIKit
+
+class TMDBInitialInitialMoviesCell:UICollectionViewCell,
+                                   UICollectionViewDataSource,
+                                   UICollectionViewDelegate,
+                                   UICollectionViewDelegateFlowLayout
+{
+    private let movieCellIdentifier = "movieCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let moviesCollectionView:UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
+    
+    func setupViews() {
+        
+        moviesCollectionView.dataSource = self
+        moviesCollectionView.delegate = self
+        
+        moviesCollectionView.register(MovieCell.self, forCellWithReuseIdentifier:movieCellIdentifier)
+                
+        addSubview(moviesCollectionView)
+        
+        NSLayoutConstraint.activate([
+            moviesCollectionView.leftAnchor.constraint(equalTo: self.leftAnchor),
+            moviesCollectionView.topAnchor.constraint(equalTo: self.topAnchor),
+            moviesCollectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            moviesCollectionView.rightAnchor.constraint(equalTo: self.rightAnchor),
+        ])
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: movieCellIdentifier, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: moviesCollectionView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+    }
+}
---
+++ Exam/TMDBInitialMovieCell.swift
@@ -0,0 +1,61 @@
+//
+//  TMDBInitialMovieCell.swift
+//  Exam
+//
+//  Created by Carlos Caraccia on 08/05/2022.
+//
+
+import UIKit
+
+class MovieCell:UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var movieImage:UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "fakeImage")
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = 16
+        iv.layer.masksToBounds = true
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    lazy var movieTitleLabel:UILabel = {
+        let label = UILabel()
+        label.text = "The adam project"
+        label.font = .systemFont(ofSize: 12, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setUpViews() {
+        
+        addSubview(movieImage)
+        addSubview(movieTitleLabel)
+        
+        NSLayoutConstraint.activate([
+            movieImage.leftAnchor.constraint(equalTo: self.leftAnchor),
+            movieImage.topAnchor.constraint(equalTo: self.topAnchor),
+            movieImage.bottomAnchor.constraint(equalTo: self.movieTitleLabel.topAnchor, constant: 8),
+            movieImage.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8),
+            movieImage.heightAnchor.constraint(equalToConstant: self.frame.height - 32)
+        ])
+        
+        NSLayoutConstraint.activate([
+           movieTitleLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 4),
+           movieTitleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+
+    }
+    
+}
+
+
